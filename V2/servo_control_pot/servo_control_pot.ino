595e38a9c989b894444a661e3ac810189327cdef
AnnieChesnais <anniechesnais94@gmail.com>
2018-03-10 09:43:04 +0100

    Arduino code for exoskeleton
    Arduino code for exoskeleton. Developed at HackForGood Salamanca 2018 by BionicLab Team

---
+++ V2/servo_control_pot/servo_control_pot.ino
@@ -0,0 +1,25 @@
+#include <Servo.h> 
+int potPin = 0;
+int servoPin = 6;
+Servo myservo;
+
+void setup() {
+  Serial.begin(19200);
+  myservo.attach(servoPin);
+  myservo.writeMicroseconds(1500);
+}
+
+void loop() {
+  
+  float potVal = analogRead(potPin);
+  float x = map(potVal, 0, 1023, 0, 300);
+  float y = map(potVal, 0, 1023, 500, 2500);
+  
+  Serial.print("Pot_Degrees: ");
+  Serial.print(x);
+  Serial.print("\tServo_val: ");    
+  Serial.println(y);
+
+  myservo.writeMicroseconds(y);
+
+}
